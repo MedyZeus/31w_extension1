56b6923a06f73009759a32aa4c838f9d4e7bd59a
Mahdi Nadir <mehdinip@gmail.com>
2022-06-22 19:33:11 -0400

    ajouter ex1_31w.php

--- ex1_31w.php
+++ ex1_31w.php
@@ -1,5 +1,21 @@
 <?php
     /**
      * Plugin Name: Extension de l'exercice 1
+     * Author: Mahdi NADIR
+     * Description: Une première extension pour l'exercice 1
+     * Author URI: https://github.com/MedyZeus/31w_extension1
      */
+?>
+<?php
+function fonction_generale( ) {
+    function mon_31w_enqueue() {
+        wp_enqueue_style(  'mon-31w_enqueue-style', 
+                            get_stylesheet_uri(), 
+                            array(), 
+                            _S_VERSION );
+    }
+    add_action( 'wp_enqueue_scripts', 'mon_31w_enqueue' );
+}
+
+    add_action('init', "fonction_generale")
 ?>
